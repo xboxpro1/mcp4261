ef52651ca4c5f3d1905affeef0e11b72d9276ad2
Martin <xboxpro1@gmail.com>
2016-06-01 13:58:18 +0200

    Update mcp4261.ino

--- example/mcp4261.ino
+++ example/mcp4261.ino
@@ -1,16 +1,18 @@
 #include "SPI.h" 
 #include "MCP4261.h" 
 
+#define on LOW
+#define off HIGH
 
-MCP4261 volumenControl = MCP4261(10);
+MCP4261 poti = MCP4261(10, 11, 12);
 
 int incomingByte = 0; 
 
 void setup(){
- Serial.begin(9600);
+ Serial.begin(115200);
  
- volumenControl.setW0Pos(10);
- volumenControl.setW1Pos(10);
+ poti.setW0Pos(10);
+ poti.setW1Pos(10);
 
 }
 
@@ -22,91 +24,49 @@ void loop()
 
    if(incomingByte == 'g'){
         Serial.print("Left: ");
-        Serial.print(volumenControl.getW0Pos());
+        Serial.print(poti.getW0Pos());
         Serial.print("  -  Rigth: ");        
-        Serial.println(volumenControl.getW1Pos());     
+        Serial.println(poti.getW1Pos());     
    }
-
-
-   if(incomingByte == '+'){
-      volumenControl.incW0();
-      volumenControl.incW1();
-     
-   }
-
-   if(incomingByte == '-'){
-      volumenControl.decW0();
-      volumenControl.decW1();
-   }
-
-
 
    if(incomingByte == '0'){
-      volumenControl.setW0Pos(0);
-      volumenControl.setW1Pos(1);
+      poti.setW0Pos(0);
+      poti.setW1Pos(1);
    }
 
    if(incomingByte == '1'){
-      volumenControl.setW0Pos(25);
-      volumenControl.setW1Pos(26);
+      poti.setW0Pos(25);
+      poti.setW1Pos(26);
    }
 
    if(incomingByte == '2'){
-      volumenControl.setW0Pos(100);
-      volumenControl.setW1Pos(101);
+      poti.setW0Pos(100);
+      poti.setW1Pos(101);
    }
 
    if(incomingByte == '3'){
-      volumenControl.setW0Pos(150);
-      volumenControl.setW1Pos(151);
+      poti.setW0Pos(150);
+      poti.setW1Pos(151);
    }
 
    if(incomingByte == '4'){
-      volumenControl.setW0Pos(200);
-      volumenControl.setW1Pos(201);
+      poti.setW0Pos(200);
+      poti.setW1Pos(201);
    }
 
 
    if(incomingByte == '5'){
-      volumenControl.setW0Pos(250);
-      volumenControl.setW1Pos(251);
-   }
-
-
-   if(incomingByte == 's'){
-      volumenControl.saveW0Pos(3);
-      volumenControl.saveW1Pos(4);
-   }
-   
-   
-   if(incomingByte == 'r'){
-      volumenControl.restoreW0Pos(3);
-      volumenControl.restoreW1Pos(4);
-   }
-
-   if(incomingByte == 'M'){
-     volumenControl.mute(true);
-     
-   }
-
-   if(incomingByte == 'm'){
-     volumenControl.mute(false);
-     
+      poti.setW0Pos(250);
+      poti.setW1Pos(251);
    }
 
    if(incomingByte == 't'){
-      uint16_t tcon = volumenControl.readTcon();
+      uint16_t tcon = poti.readTcon();
        Serial.print("  -  Tcon: ");        
        Serial.println(tcon,BIN);     
    }
 
-   if(incomingByte == 'x'){
-     volumenControl.writeTcon(B00000000);
-   }
-
-   if(incomingByte == 'y'){
-     volumenControl.writeTcon(B11111111);
-   }
+  
 
  }
 

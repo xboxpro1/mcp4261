63db5b348278aed0fff5a5928dcc17e5a1968d74
Martin <xboxpro1@gmail.com>
2016-06-01 18:43:58 +0200

    Update mcp4261.ino

--- example/mcp4261.ino
+++ example/mcp4261.ino
@@ -6,7 +6,7 @@
 
 const int sd_poti = 9;                                   // SHDN Poti
 const int cs_poti = 10;                                  // CS Poti
-const int rw_poti = 11;                                  // RST / WP Poti
+const int rw_poti = 11;                                  // WP Poti
 
 MCP4261 poti = MCP4261(cs_poti, sd_poti, rw_poti);
 
@@ -17,7 +17,28 @@ void setup(){
  
  poti.setW0Pos(10);
  poti.setW1Pos(15);
+ help();
 
+}
+
+void help(){
+    Serial.println("MCP4261 - Test");
+    Serial.println("g -> get Wiper Value");
+    Serial.println("0 -> set Wiper 0/5");
+    Serial.println("1 -> set Wiper 25/30");
+    Serial.println("2 -> set Wiper 100/105");
+    Serial.println("3 -> set Wiper 150/155");
+    Serial.println("4 -> set Wiper 200/205");
+    Serial.println("5 -> set Wiper 250/255");
+    Serial.println("t -> read TCON-Register");
+    Serial.println("s -> read Setup-Register");
+    Serial.println("e -> enable Poti");
+    Serial.println("x -> disable Poti");
+    Serial.println("a -> W0-ON W1-OFF");
+    Serial.println("b -> W0-OFF W1-ON");
+    Serial.println("c -> W0-ON W1-ON");
+    Serial.println("d -> W0-OFF W1-OFF");
+    Serial.println("h -> this Text");
 }
 
 void loop()
@@ -66,7 +87,7 @@ void loop()
 
    if(incomingByte == 't'){
       uint16_t tcon = poti.readTcon();
-       Serial.print("Tcon: ");        
+       Serial.print("TCON: ");        
        Serial.println(tcon,BIN);     
    }
 
@@ -79,19 +100,23 @@ void loop()
    }
    
    if(incomingByte == 'a'){
-    poti.wiperOn(on, off);
+    poti.wiperOn(true, false);
    }
    
    if(incomingByte == 'b'){
-    poti.wiperOn(off, on);
+    poti.wiperOn(false, true);
    }
    
    if(incomingByte == 'c'){
-    poti.wiperOn(on, on);
+    poti.wiperOn(true, true);
    }
    
    if(incomingByte == 'd'){
-    poti.wiperOn(off, off);
+    poti.wiperOn(false, false);
+   }
+   
+   if(incomingByte == 'h'){
+    help();
    }
    
  }

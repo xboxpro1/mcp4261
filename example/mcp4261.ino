914421088d4dda3861ab532772b958814d07364a
Martin <xboxpro1@gmail.com>
2016-06-01 18:22:06 +0200

    Update mcp4261.ino

--- example/mcp4261.ino
+++ example/mcp4261.ino
@@ -4,7 +4,11 @@
 #define on LOW
 #define off HIGH
 
-MCP4261 poti = MCP4261(10, 11, 12);
+const int sd_poti = 9;                                   // SHDN Poti
+const int cs_poti = 10;                                  // CS Poti
+const int rw_poti = 11;                                  // RST / WP Poti
+
+MCP4261 poti = MCP4261(cs_poti, sd_poti, rw_poti);
 
 int incomingByte = 0; 
 
@@ -12,7 +16,7 @@ void setup(){
  Serial.begin(115200);
  
  poti.setW0Pos(10);
- poti.setW1Pos(10);
+ poti.setW1Pos(15);
 
 }
 
@@ -31,38 +35,38 @@ void loop()
 
    if(incomingByte == '0'){
       poti.setW0Pos(0);
-      poti.setW1Pos(1);
+      poti.setW1Pos(5);
    }
 
    if(incomingByte == '1'){
       poti.setW0Pos(25);
-      poti.setW1Pos(26);
+      poti.setW1Pos(30);
    }
 
    if(incomingByte == '2'){
       poti.setW0Pos(100);
-      poti.setW1Pos(101);
+      poti.setW1Pos(105);
    }
 
    if(incomingByte == '3'){
       poti.setW0Pos(150);
-      poti.setW1Pos(151);
+      poti.setW1Pos(155);
    }
 
    if(incomingByte == '4'){
       poti.setW0Pos(200);
-      poti.setW1Pos(201);
+      poti.setW1Pos(205);
    }
 
 
    if(incomingByte == '5'){
       poti.setW0Pos(250);
-      poti.setW1Pos(251);
+      poti.setW1Pos(255);
    }
 
    if(incomingByte == 't'){
       uint16_t tcon = poti.readTcon();
-       Serial.print("  -  Tcon: ");        
+       Serial.print("Tcon: ");        
        Serial.println(tcon,BIN);     
    }
 

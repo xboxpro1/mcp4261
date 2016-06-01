c324b5064c12b8e834a0e7be8a3dca4efb819aff
Martin <xboxpro1@gmail.com>
2016-06-01 10:31:34 +0200

    Update MCP4261.h

--- MCP4261.h
+++ MCP4261.h
@@ -15,14 +15,6 @@ const static uint8_t WRITE_W0NV  = B00100000;
 const static uint8_t WRITE_W1NV  = B00110000;
 const static uint8_t READ_W0NV  = B00101100;
 const static uint8_t READ_W1NV  = B00111100;   
-const static uint8_t INC_W0  = B00000100;
-const static uint8_t INC_W1  = B00010100;
-const static uint8_t DEC_W0  = B00001000;
-const static uint8_t DEC_W1  = B00011000;
-const static uint8_t W0ON_W1ON  = B01110111;
-const static uint8_t W0OFF_W1OFF  = B00000000;
-const static uint8_t W0ON_W1OFF  = B00000111;
-const static uint8_t W0OFF_W1ON  = B01110000;
 
 const static uint8_t TCON_READ  =  B01001100;
 const static uint8_t TCON_WRITE  = B01000000;
@@ -38,27 +30,19 @@ const static uint8_t TCON_WRITE  = B01000000;
  const static uint8_t EEPROM_R[] = {B01101111,B01111111,B10001111,B10011111,B10101111,B10111111,B11001111,B11011111,B11101111,B11111111};
 
 
-
-
-
 class MCP4261{
 
 public:
 
 MCP4261(int cs);
 int  getW0Pos();
+int  getW0NVPos();
 int  getW1Pos();
-void incW0();
-void incW1();
-void decW0();
-void decW1();
+int  getW1NVPos();
 void setW0Pos(uint16_t pos);
+void setW0NVPos(uint16_t pos);
 void setW1Pos(uint16_t pos);
-void saveW0Pos(int num);
-void saveW1Pos(int num);
-void  restoreW0Pos(int num);
-void  restoreW1Pos(int num);
-void  mute(boolean mute);
+void setW1NVPos(uint16_t pos);
 void MCP4261::wiperOn(boolean w0_on, boolean w1_on);
 
 uint16_t readTcon();

45f3f51efe5ff3cd10a4e14cc38a44d79334b999
Martin <xboxpro1@gmail.com>
2016-06-08 09:40:45 +0200

    Update mcp4261.cpp

--- mcp4261.cpp
+++ mcp4261.cpp
@@ -2,111 +2,130 @@
 
 
 MCP4261::MCP4261(int chipSelect){
-  cs = chipSelect;
-  pe = 99;
-  nv = 99;
-  pinMode(cs, OUTPUT); 
-  digitalWrite(cs, HIGH); 
+  _cs = chipSelect;
+  _pe = 99;
+  _nv = 99;
+  pinMode(_cs, OUTPUT); 
+  digitalWrite(_cs, HIGH); 
   SPI.begin();
 }
+
 MCP4261::MCP4261(int chipSelect, int potE, int proNV){
-  cs = chipSelect;
-  pe = potE;
-  nv = proNV;
-  pinMode(cs, OUTPUT); 
-  digitalWrite(cs, HIGH); 
-  pinMode(pe, OUTPUT); 
-  digitalWrite(pe, HIGH);
-  pinMode(nv, OUTPUT); 
-  digitalWrite(nv, HIGH);
+  _cs = chipSelect;
+  _pe = potE;
+  _nv = proNV;
+  pinMode(_cs, OUTPUT); 
+  digitalWrite(_cs, HIGH); 
+  pinMode(_pe, OUTPUT); 
+  digitalWrite(_pe, HIGH);
+  pinMode(_nv, OUTPUT); 
+  digitalWrite(_nv, HIGH);
   SPI.begin();
 }
+
 void MCP4261::write(byte cmd_byte, byte data_byte)
 {
   #ifdef SPI_HAS_TRANSACTION
     SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0));
   #endif
-  digitalWrite(cs, LOW);  
+  digitalWrite(_cs, LOW);  
   byte high_byte = SPI.transfer(cmd_byte);
   byte low_byte  = SPI.transfer(data_byte);
-  digitalWrite(cs, HIGH);
+  digitalWrite(_cs, HIGH);
   #ifdef SPI_HAS_TRANSACTION
     SPI.endTransaction();
   #endif
   bool result = ~low_byte;
 }
+
 uint16_t MCP4261::read(byte cmd_byte)
 {
   #ifdef SPI_HAS_TRANSACTION
     SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0));
   #endif
-  digitalWrite(cs, LOW); 
+  digitalWrite(_cs, LOW); 
   byte high_byte = SPI.transfer(cmd_byte);
   byte low_byte  = SPI.transfer(0xFF);
-  digitalWrite(cs, HIGH);
+  digitalWrite(_cs, HIGH);
   #ifdef SPI_HAS_TRANSACTION
     SPI.endTransaction();
   #endif
   return byte2uint16(high_byte, low_byte);
 }
+
 uint16_t MCP4261::byte2uint16(byte high_byte, byte low_byte)
 {
   return (uint16_t)high_byte<<8 | (uint16_t)low_byte;
 }
+
 int  MCP4261::getW0Pos(){
  int pos =   0x01FF &  read(READ_W0);
  return pos;
 }
+
 int  MCP4261::getW0NVPos(){
  int pos =   0x01FF &  read(READ_W0NV);
  return pos;
 }
+
 int  MCP4261::getW1Pos(){
  int pos =   0x01FF &  read(READ_W1);
  return pos;
 }
+
 int  MCP4261::getW1NVPos(){
  int pos =   0x01FF &  read(READ_W1NV);
  return pos;
 }
+
 void MCP4261::setW0Pos(uint16_t pos){
       write(WRITE_W0, pos);
 }
+
 void MCP4261::setW0NVPos(uint16_t pos){
       write(WRITE_W0NV, pos);
       delay(5);
 }
+
 void MCP4261::setW1Pos(uint16_t pos){
       write(WRITE_W1, pos);
 }
+
 void MCP4261::setW1NVPos(uint16_t pos){
       write(WRITE_W1NV, pos);
       delay(5);
 }
+
 void MCP4261::enable(){
       if(pe != 99){
-        digitalWrite(pe, LOW);
+        digitalWrite(_pe, LOW);
       }
 }
+
 void MCP4261::disable(){
       if(pe != 99){
-        digitalWrite(pe, HIGH);
+        digitalWrite(_pe, HIGH);
       }
 }
+
 void MCP4261::protectNV(int nvp){
       if(nv != 99){
-        digitalWrite(nv, nvp);
+        digitalWrite(_nv, nvp);
       }
 }
+
 uint16_t MCP4261::readTcon(){
   return  read(TCON_READ);
 }
+
 void MCP4261::writeTcon(uint16_t tcon){
     write(TCON_WRITE,tcon);
 }
+
 uint16_t MCP4261::readStatus(){
   return  read(STATUS_READ);
 }
+
 void MCP4261::wiperOn(bool w0_on, bool w1_on){
   uint16_t won = 0x00;
   if(w1_on){

2d07a3cf253675b0a128ac6878d3a75bc1572f87
Martin <xboxpro1@gmail.com>
2016-06-15 11:56:50 +0200

    Update mcp4261.h

--- mcp4261.h
+++ mcp4261.h
@@ -4,20 +4,17 @@
 #include <Arduino.h>
 #include <SPI.h>
 
-const static uint8_t WRITE_W0  = B00000000;
-const static uint8_t WRITE_W1  = B00010000;
-const static uint8_t READ_W0  = B00001100;
-const static uint8_t READ_W1  = B00011100; 
-const static uint8_t WRITE_W0NV  = B00100000;
-const static uint8_t WRITE_W1NV  = B00110000;
-const static uint8_t READ_W0NV  = B00101100;
-const static uint8_t READ_W1NV  = B00111100;   
-
-const static uint8_t TCON_READ  =  B01001100;
-const static uint8_t TCON_WRITE  = B01000000;
-
-const static uint8_t STATUS_READ  =  B01011100;
-
+const static uint8_t WRITE_W0  = 0x00;
+const static uint8_t WRITE_W1  = 0x10;
+const static uint8_t WRITE_W0NV  = 0x20;
+const static uint8_t WRITE_W1NV  = 0x30;
+const static uint8_t TCON_WRITE  = 0x40;
+const static uint8_t READ_W0  = 0x0C;
+const static uint8_t READ_W1  = 0x1C; 
+const static uint8_t READ_W0NV  = 0x2C;
+const static uint8_t READ_W1NV  = 0x3C;   
+const static uint8_t TCON_READ  =  0x4C;
+const static uint8_t STATUS_READ  =  0x5C;
 
 class MCP4261{
 
